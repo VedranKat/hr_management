9b6f249d646f3003ea86d654795f16429d42b3d5
VedranKat <vedstandard@runbox.com>
2020-08-19 23:41:39 +0200

    forma za dodavanje

--- src/Components/AdminAdd/AdminAdd.js
+++ src/Components/AdminAdd/AdminAdd.js
@@ -1,6 +1,6 @@
 import React from "react";
 import AdminHeader from "../AdminHeader/AdminHeader.js";
-import AdminAddForm from "../AdminAddForm/AdminAddForm.js";
+import AddForm from "../../Containers/AddForm/AddForm.js";
 import styles from "./AdminAdd.module.css";
 
 
@@ -10,7 +10,7 @@ const AdminAdd=()=>{
 return(
     <div>
         <AdminHeader></AdminHeader>
-        <AdminAddForm></AdminAddForm>        
+        <AddForm></AddForm>        
     </div>
 )
 
---
+++ src/Containers/AddForm/AddForm.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { addCard } from '../../redux/actions';
+import { postCard } from "../../services";
+import styles from './AddForm.module.css';
+
+class AddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ime: '',
+      prezime: '',
+      radnoMjesto: '',
+      godinaRodjenja: ''
+    }
+  }
+
+  handleSubmit = async (event) => {
+    
+    try {   
+        await postCard(this.state);
+        this.props.actions.addCard(this.state);            
+    } catch (error) {
+      console.log(error);
+    }
+
+    this.setState({ ime: "",
+    prezime: "",
+    radnoMjesto: "",
+    godinaRodjenja: "" });
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    return (      
+      <form className={styles.form} onSubmit={this.handleSubmit}>
+        <input
+          className={styles.input}   
+          type="text"
+          name="ime"
+          onChange={this.handleChange}
+          required
+        />
+        <input
+          className={styles.input}          
+          type="text"
+          name="prezime"
+          onChange={this.handleChange}
+          required
+        />
+        <input
+          className={styles.input}          
+          type="text"
+          name="radnoMjesto"
+          onChange={this.handleChange}
+          required
+        />
+        <input
+          className={styles.input}          
+          type="date"          
+          name="godinaRodjenja"
+          onChange={this.handleChange}
+          required
+        />
+        <button type="submit" className={styles.button}>Add card</button>
+      </form>      
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    cards: dispatch.cards.cards
+  };
+}
+
+export default connect(  
+  mapDispatchToProps
+)(AddForm);
