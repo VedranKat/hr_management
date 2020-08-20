27d79849e2b73038a0a2b61b21aaec8ad00d00a7
VedranKat <vedstandard@runbox.com>
2020-08-20 22:16:51 +0200

    push na komp

--- src/Containers/AddForm/AddForm.js
+++ src/Containers/AddForm/AddForm.js
@@ -39,21 +39,24 @@ class AddForm extends Component {
     return (      
       <form className={styles.form} onSubmit={this.handleSubmit}>
         <input
-          className={styles.input}   
+          className={styles.input}  
+          placeholder="ime" 
           type="text"
           name="ime"
           onChange={this.handleChange}
           required
         />
         <input
-          className={styles.input}          
+          className={styles.input}     
+          placeholder="prezime"     
           type="text"
           name="prezime"
           onChange={this.handleChange}
           required
         />
         <input
-          className={styles.input}          
+          className={styles.input}   
+          placeholder="radnoMjesto"       
           type="text"
           name="radnoMjesto"
           onChange={this.handleChange}
